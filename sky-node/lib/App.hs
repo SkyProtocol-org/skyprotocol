a39ab705d55ecd59959685bed8884402b763b111
Yaroslav Kozhevnikov <yroslav541@gmail.com>
2024-11-15 20:00:38 +0200

    feat: basic app structure

--- sky-node/lib/App.hs
+++ sky-node/lib/App.hs
@@ -1,16 +1,48 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module App (runApp) where
 
 -- import App.Error
-import Config (AppConfig)
+import Config (AppConfig (..))
+import Control.Concurrent.STM (newTVarIO)
+import Control.Concurrent.STM.TVar (TVar)
 import System.Log.FastLogger (LoggerSet, pushLogStrLn, toLogStr)
 
--- initAppState :: IO ()
--- initAppState = pure ()
+-- | Aux data structure to keep info about 'Peer'.
+newtype Peer = Peer
+  { -- | Id of the 'Peer'
+    id :: String
+  }
+  deriving (Show)
 
+-- | Signalizes if the node needs to shutdown or continue running.
+data Shutdown = Continue | Shutdown deriving (Show)
+
+-- | State of the node.
+data AppState = AppState
+  { -- | Messages.
+    messages :: TVar [Int],
+    -- | List of peers that are connected to this node.
+    peers :: [Peer],
+    -- | Marker for graceful shutdown.
+    continue :: Shutdown
+  }
+
+-- | Initializes default node state
+initAppState :: IO AppState
+initAppState = do
+  let peers = []
+      continue = Continue
+  messages <- newTVarIO []
+  pure $ AppState {..}
+
+-- | Run the application
 runApp :: AppConfig -> LoggerSet -> IO ()
 runApp config logger = do
-  -- state <- initAppState
-  print config
-  pushLogStrLn logger $ toLogStr "Starting Sky Node"
+  state <- initAppState
+  pushLogStrLn logger . toLogStr $ "Starting Sky Node on port " <> config.port
+  runServer config state logger
 
--- runNode config state logger
+runServer :: AppConfig -> AppState -> LoggerSet -> IO ()
+runServer config state logger = undefined
--- sky-node/lib/App/Error.hs
+++ sky-node/lib/App/Error.hs
@@ -1,6 +1,9 @@
 module App.Error where
 
+-- | Top-level error type
 data AppError
-  = HandlerError String
-  | AppError String
+  = -- | Constructor for error propagation from the handler level. Used for errors that must terminate the execution of the node.
+    HandlerError String
+  | -- | Constructor for errors in the node itself.
+    AppError String
   deriving (Show)
