020219e7a846c7e5aecaf6d1ab7ca4c7825b50bd
Yaroslav Kozhevnikov <yroslav541@gmail.com>
2024-11-30 14:56:19 +0200

    feat: integrating effectful[wip]

---
+++ sky-node/lib/Types.hs
@@ -0,0 +1,24 @@
+module Types where
+
+import qualified Data.ByteString as BS
+import Data.IntMap.Strict (IntMap)
+
+newtype TopicId = TopicId {topicId :: BS.ByteString} deriving (Show, Eq)
+
+newtype TopicMetaData = TopicMetaData {topicMetaId :: TopicId} deriving (Show, Eq)
+
+data Topic = Topic
+  { topicMeta :: TopicMetaData,
+    topicMessages :: IntMap BlockData
+  }
+  deriving (Show, Eq)
+
+data Block
+
+newtype BlockData = BlockData {blockData :: BS.ByteString} deriving (Show, Eq)
+
+newtype Certificate a = Certificate {cert :: BS.ByteString} deriving (Show, Eq)
+
+-- TODO for now it's a stub
+makeBlockCertificate :: BlockData -> Certificate Block
+makeBlockCertificate = undefined
