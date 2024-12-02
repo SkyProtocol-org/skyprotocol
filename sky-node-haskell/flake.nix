3ed4f276f555c69e8a79e6e2a9254c8d989028c1
Yaroslav Kozhevnikov <yroslav541@gmail.com>
2024-12-02 11:50:28 +0200

    feat: some package management

--- sky-node-haskell/flake.nix
+++ sky-node-haskell/flake.nix
@@ -2,12 +2,16 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    merkle-trie = {
+      url = "github:Skyprotocol-org/skyprotocol/issue-22/haskell-implementation-of-sky-node?dir=merkle-patricia-trie";
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, merkle-trie, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
       overlay = final: prev: {
         sky-node = final.callCabal2nix "sky-node" ./. { };
+        merkle-trie = final.callCabal2nix "merkle-patricia-trie" merkle-trie.src { };
       };
       haskPkgs = pkgs.haskellPackages.extend overlay;
     in
@@ -15,6 +19,7 @@
       devShells.default = haskPkgs.shellFor {
         packages = p: [
           p.sky-node
+          p.merkle-patricia-trie
         ];
         nativeBuildInputs = with haskPkgs; [
           cabal-install
