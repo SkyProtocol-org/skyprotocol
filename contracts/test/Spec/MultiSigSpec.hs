f0069675faa43bb04f59d9c98b2b14bdd9d6e4a8
Manuel <msimoni+sky@gmail.com>
2024-12-02 12:50:14 +0000

    Add second sig

--- contracts/test/Spec/MultiSigSpec.hs
+++ contracts/test/Spec/MultiSigSpec.hs
@@ -25,16 +25,34 @@ dh2 = DataHash $ hex "BABE"
 pk1 :: PubKey
 pk1 = PubKey $ hex "3363A313E34CF6D3B9E0CE44AED5A54567C4302B873DD69EC7F37B9E83AABF65"
 
-sig1 :: BuiltinByteString
+sig1 :: BuiltinByteString -- signs dh1 with sk1
 sig1 = hex "184E401E7EA7C7E2F9B4186DEDF953437F81BD2664D1FBDE525264A4D08BFD79D81877376F1E63CE64DF46C5F1FD93CDF3B05B8B6076A6ADC05F36C81F62A500"
 
 ss1 :: SingleSig
 ss1 = SingleSig pk1 sig1
 
+-- sk2: B2CB983D9764E7CC7C486BEBDBF1C2AA726EF78BB8BC1C97E5139AE58165A00F
+pk2 :: PubKey
+pk2 = PubKey $ hex "42FB07466D301CA2CC2EFF2FD93A67EB1EBBEC213E6532A04DC82BE6A41329AE"
+
+sig2 :: BuiltinByteString -- signs dh1 with sk2
+sig2 = hex "B7837207523B267F5B9AA0117C02773474A5F9F9FC4D6F48AEB2DC1B7A5796E60EE17C1F5C81D43C1973C0536932FB328897B341A7F8B3B86CB66ACEF459B405"
+
+ss2 :: SingleSig 
+ss2 = SingleSig pk2 sig2
+
 spec :: Spec
 spec = do
   describe "Single Sig operations" $ do
-    it "single sig should be valid" $ do
+    
+    it "single sig 1 should be valid" $ do
       (singleSigValid dh1 ss1) `shouldBe` True
-    it "single sig should not be valid for wrong hash" $ do
+
+    it "single sig 2 should be valid" $ do
+      (singleSigValid dh1 ss2) `shouldBe` True
+
+    it "single sig 1 should not be valid for wrong hash" $ do
       (singleSigValid dh2 ss1) `shouldBe` False
+
+    it "single sig 2 should not be valid for wrong hash" $ do
+      (singleSigValid dh2 ss2) `shouldBe` False
