dd4c51440aa9481b0c8bea9b7687ea798a7f79e7
Yaroslav Kozhevnikov <yroslav541@gmail.com>
2024-12-11 16:38:29 +0200

    feat: small additions

--- merkle-patricia-trie/lib/Data/Trie.hs
+++ merkle-patricia-trie/lib/Data/Trie.hs
@@ -1,20 +1,22 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module Data.Trie (Trie (..), lookup, insert) where
 
 import Data.Bits
-import Prelude (Bool, Enum, Eq, Integral, Maybe (..), Num, Ord, Real, not, otherwise, undefined, ($), (-), (.), (==))
+import Data.Kind (Type)
+import Prelude hiding (lookup)
 
-data Trie k a
+data Trie (f :: Type -> Type) k a
   = Empty
-  | Leaf k a
-  | Branch k k (Trie k a) (Trie k a)
+  | Leaf k (f a)
+  | Branch k k (Trie f k a) (Trie f k a)
 
--- the idea is that you create a datatype for key, which
 class (Bits k, Num k, Integral k) => TrieKey k where
+  -- | Discards bits before/after(depending on endianess) branching bit
   mask :: k -> k -> k
+
+  -- | Leaves only the branching bit(so key 0b1001101 becomes 0b0001000, if 4th bit was branching)
   branchingBit :: k -> k -> k
 
 newtype LEK a = LittleEndian a deriving (Enum, Eq, Ord, Real, Bits, Num, Integral)
@@ -34,7 +36,7 @@ instance (Bits k, Num k, Integral k) => TrieKey (BEK k) where
   -- since it needs more args than just a and b(it also needs prefixes)
   branchingBit _ _ = undefined
 
-lookup :: (TrieKey k) => k -> Trie k a -> Maybe a
+lookup :: (TrieKey k, Functor f) => k -> Trie f k a -> Maybe (f a)
 lookup _ Empty = Nothing
 lookup k (Leaf key a) = if k == key then Just a else Nothing
 lookup k (Branch key prefix t1 t2)
@@ -48,7 +50,7 @@ matchPrefix k prefix key = mask k key == prefix
 zeroBit :: (TrieKey k) => k -> k -> Bool
 zeroBit a b = (a .&. b) == 0
 
-join :: (TrieKey k) => k -> Trie k a -> k -> Trie k a -> Trie k a
+join :: (TrieKey k) => k -> Trie f k a -> k -> Trie f k a -> Trie f k a
 join p1 t1 p2 t2 =
   if zeroBit p1 commonPrefix
     then Branch (mask p1 commonPrefix) commonPrefix t1 t2
@@ -57,19 +59,18 @@ join p1 t1 p2 t2 =
     commonPrefix = branchingBit p1 p2
 
 -- maybe conflict func must be a Monoid?
-insert :: forall k v. (TrieKey k) => (v -> v -> v) -> k -> v -> Trie k v -> Trie k v
+insert :: (TrieKey k, Functor f, Applicative f) => (v -> v -> v) -> k -> v -> Trie f k v -> Trie f k v
 insert conflict k v t = go t
   where
-    go :: Trie k v -> Trie k v
-    go Empty = Leaf k v
+    go Empty = Leaf k $ pure v
     go (Leaf key val) =
       if k == key
-        then Leaf k $ conflict v val
-        else join k (Leaf k v) key t
+        then Leaf k $ conflict v <$> val
+        else join k (Leaf k $ pure v) key t
     go (Branch key prefix t1 t2) =
       if matchPrefix k prefix key
         then
           if zeroBit k key
             then Branch key prefix (go t1) t2
             else Branch key prefix t1 (go t2)
-        else join k (Leaf k v) prefix t
+        else join k (Leaf k $ pure v) prefix t
