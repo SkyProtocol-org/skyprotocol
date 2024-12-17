ac927a7460816fdc6b4c771e08366fe1ce18359b
Yaroslav Kozhevnikov <yroslav541@gmail.com>
2024-12-17 16:26:54 +0200

    feat: refactoring of trie&zipper implementations

--- merkle-patricia-trie/lib/Data/Trie/Internal.hs
+++ merkle-patricia-trie/lib/Data/Trie/Internal.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -11,25 +13,16 @@ import Data.WideWord (Word256)
 import Data.Word (Word8)
 import Prelude hiding (lookup)
 
-data Trie' (w :: Type -> Type) k h a
+data Trie' k h a
   = Empty
-  | Leaf k (w a)
-  | Branch k k (Trie' w k h a) (Trie' w k h a)
-  deriving (Functor)
-
--- | This will allow us to enforce the height of the trie depending on it's key
-type family TrieHeight key where
-  TrieHeight (LEK Word256) = Word8
-  TrieHeight (BEK Word256) = Word8
-
--- | This will ensure, that when someone tries to create a Trie, the height will be automagically supplied
-type Trie w k a = Trie' w k (TrieHeight k) a
-
-type LittleTrie w k a = Trie w (LEK k) a
-
-type BigTrie w k a = Trie w (BEK k) a
+  | Leaf {key :: k, val :: a}
+  | Branch {bBit :: h, pref :: k, left :: Trie' k h a, right :: Trie' k h a}
+  deriving (Show, Eq, Functor)
 
 class (Bits k, Num k, Integral k) => TrieKey k where
+  -- | This will allow us to enforce the height of the trie depending on it's key
+  type TrieHeight k :: Type
+
   -- | Discards bits before/after(depending on endianess) branching bit
   mask :: k -> k -> k
 
@@ -42,24 +35,7 @@ class (Bits k, Num k, Integral k) => TrieKey k where
   zeroBit :: k -> k -> Bool
   zeroBit a b = (a .&. b) == 0
 
-newtype LEK a = LittleEndian a deriving (Enum, Eq, Ord, Real, Bits, Num, Integral)
-
-newtype BEK a = BigEndian a deriving (Enum, Eq, Ord, Real, Bits, Num, Integral)
-
-instance (Bits k, Num k, Integral k) => TrieKey (LEK k) where
-  mask (LittleEndian a) (LittleEndian b) = LittleEndian $ a .&. (b - 1)
-  branchingBit (LittleEndian a) (LittleEndian b) = LittleEndian . lowestBit $ a `xor` b
-    where
-      lowestBit x = x .&. complement x
-
-instance (Bits k, Num k, Integral k) => TrieKey (BEK k) where
-  mask (BigEndian a) (BigEndian b) = BigEndian $ (a .|. (b - 1)) .&. complement b
-
-  -- TODO figure out how to properly implement branchingBit for BigEndian
-  -- since it needs more args than just a and b(it also needs prefixes)
-  branchingBit _ _ = undefined
-
-join :: (TrieKey k) => k -> Trie w k a -> k -> Trie w k a -> Trie w k a
+join :: (TrieKey k) => k -> Trie' k h a -> k -> Trie' k h a -> Trie' k h a
 join p1 t1 p2 t2 =
   if zeroBit p1 bBit
     then Branch bBit (mask p1 bBit) t1 t2
