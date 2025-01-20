6784e461dbb6660c40a834a214649bd4a921e30d
Manuel <msimoni+sky@gmail.com>
2025-01-20 10:39:13 +0000

    Inline data structures

--- contracts/off-chain/http-api/update-bridge.mjs
+++ contracts/off-chain/http-api/update-bridge.mjs
@@ -78,47 +78,38 @@ export async function updateBridge()
     // Create Data to Send to Bridge Contract
     //////////////////////////////////////////////////////////////////////////////
 
-    function mkDataHash(hex) { return { alternative: 0, fields: [hex] } }
-    function mkPubKey(hex) { return { alternative: 0, fields: [hex] } }
-
-    const pk1 = mkPubKey("3363A313E34CF6D3B9E0CE44AED5A54567C4302B873DD69EC7F37B9E83AABF65");
-    const pk2 = mkPubKey("42FB07466D301CA2CC2EFF2FD93A67EB1EBBEC213E6532A04DC82BE6A41329AE");
-    const pk3 = mkPubKey("22B9524D37A16C945DEEC3455D92A1EBC5AC857174F5A0A8B376517A205DCA73");
-
-    // Create main committee MultiSigPubKey
-    const msPublicKey = {
-        alternative: 0,
-        fields: [
-	    [ pk1, pk2, pk3 ],
-	    2 // Number of public keys that must sign
-        ]
-    }
-
-    // MultiSig
-    const ms = {
-        alternative: 0,
-        fields: [
-	    // [SingleSig]
-	    [ { alternative: 0, // top hash 2 sig 1
-	        fields: [ pk1, "87E894C503E40A8CB98DEB8618DC068323092871C717D4781D56FCBBE10FCD6B1965ADE766FFDFAF8F7B2964F3ED8A6066703DD9AA68F583055ED53FBA27A90E" ] },
-	      { alternative: 0, // top hash 2 sig 2
-	        fields: [ pk2, "99E3BBBCA63ECDA27ADC6ED426A695E32AA5D7185CFC16F550834919C96F7FA17E19992E6FB2D302BE8FF71CF71907F654F25727425C0F30989B4AAC7767B003" ] }
-	    ]
-        ]
-    }
-
-    const TOPHASH2 = mkDataHash("3c7dfafe47aac5454629d9280529b90b82d07ba80b89757d652bff047f0534a1"); // top hash 2
-
     // Create UpdateBridge redeemer
     const redeemer = {
         alternative: 0,
         fields: [
-	    msPublicKey,
-	    mkDataHash("9f06268167a61b7f54210ebcd0a92d9000211a41401f7827b5bf905b8fd3e263"), // main root hash 1
-	    TOPHASH2,
-	    ms
+	    {
+                alternative: 0,
+                fields: [
+                    [
+                        { alternative: 0, fields: ["3363A313E34CF6D3B9E0CE44AED5A54567C4302B873DD69EC7F37B9E83AABF65"] },
+                        { alternative: 0, fields: ["42FB07466D301CA2CC2EFF2FD93A67EB1EBBEC213E6532A04DC82BE6A41329AE"] },
+                        { alternative: 0, fields: ["22B9524D37A16C945DEEC3455D92A1EBC5AC857174F5A0A8B376517A205DCA73"] }
+                    ],
+                    2 // Number of public keys that must sign
+                ]
+            },
+	    { alternative: 0, fields: ["9f06268167a61b7f54210ebcd0a92d9000211a41401f7827b5bf905b8fd3e263"] }, // main root hash 1
+	    { alternative: 0, fields: ["3c7dfafe47aac5454629d9280529b90b82d07ba80b89757d652bff047f0534a1"] }, // top hash 2
+	    {
+                alternative: 0,
+                fields: [
+	            // [SingleSig]
+	            [ { alternative: 0, // top hash 2 sig 1
+	                fields: [ { alternative: 0, fields: ["3363A313E34CF6D3B9E0CE44AED5A54567C4302B873DD69EC7F37B9E83AABF65"] },
+                                  "87E894C503E40A8CB98DEB8618DC068323092871C717D4781D56FCBBE10FCD6B1965ADE766FFDFAF8F7B2964F3ED8A6066703DD9AA68F583055ED53FBA27A90E" ] },
+	              { alternative: 0, // top hash 2 sig 2
+	                fields: [ { alternative: 0, fields: ["42FB07466D301CA2CC2EFF2FD93A67EB1EBBEC213E6532A04DC82BE6A41329AE"] },
+                                  "99E3BBBCA63ECDA27ADC6ED426A695E32AA5D7185CFC16F550834919C96F7FA17E19992E6FB2D302BE8FF71CF71907F654F25727425C0F30989B4AAC7767B003" ] }
+	            ]
+                ]
+            }
         ]
-    }
+    };
 
     //////////////////////////////////////////////////////////////////////////////
     // Find Bridge NFT and Create Datum Updated With New Top Hash
@@ -130,7 +121,7 @@ export async function updateBridge()
     const updatedDatum = {
         alternative: 0,
         fields: [
-	    TOPHASH2
+	    { alternative: 0, fields: ["3c7dfafe47aac5454629d9280529b90b82d07ba80b89757d652bff047f0534a1"] } // tophash 2
         ]
     };
 
