701376227f303f5b09cd4b8c56b4d8acd88ed3bb
Yaroslav Kozhevnikov <yroslav541@gmail.com>
2025-01-28 14:35:22 +0200

    feat: hex encoding for merkle proof json serialization

--- merkle-patricia-trie/test/MerkleTrieSpec.hs
+++ merkle-patricia-trie/test/MerkleTrieSpec.hs
@@ -6,6 +6,7 @@
 
 module MerkleTrieSpec (spec) where
 
+import Data.Aeson (encodeFile)
 import Data.Binary (decode, encode)
 import Data.MerkleTrie
 import Data.Trie qualified as Trie
@@ -17,8 +18,19 @@ import Test.QuickCheck
 instance Trie.TrieKey Word256 where
   type TrieHeight Word256 = Word8
 
+dumpExample :: IO ()
+dumpExample = do
+  let t = Trie.insert @Word256 @String 1 "value1" $ Trie.insert 2 "value2" Trie.empty
+      proof1 = proof 1 t
+      proof2 = proof 2 t
+  case (proof1, proof2) of
+    (Just p1, Just p2) -> do
+      encodeFile "valid_proof.json" p1
+      encodeFile "invalid_proof.json" $ p2 {targetValue = "invalidValue"}
+    _ -> pure ()
+
 spec :: Spec
-spec = do
+spec = beforeAll dumpExample $ do
   describe "MerkleTrie" $ do
     it "should generate a proof, validate it, and compute the root hash correctly" $ do
       let t = Trie.insert @Word256 @String 1 "value1" $ Trie.insert 2 "value2" Trie.empty
