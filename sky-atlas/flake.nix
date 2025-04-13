8f8e5f207c719e876d31d8e242998d77922aee59
Yaroslav Kozhevnikov <yroslav541@gmail.com>
2025-04-13 20:22:32 +0300

    feat: trying to debug setup[wip]

--- sky-atlas/flake.nix
+++ sky-atlas/flake.nix
@@ -1,47 +1,118 @@
 {
-  description = "Atlas test for skyprotocol";
+
+  description = "sky-atlas";
 
   inputs = {
-    iogx = {
-      url = "github:input-output-hk/iogx";
-      inputs.hackage.follows = "hackage";
-      inputs.CHaP.follows = "CHaP";
-      inputs.haskell-nix.follows = "haskell-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixpkgs.follows = "haskell-nix/nixpkgs";
-
-    hackage = {
-      url = "github:input-output-hk/hackage.nix";
-      flake = false;
-    };
-
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     CHaP = {
       url = "github:IntersectMBO/cardano-haskell-packages?ref=repo";
       flake = false;
     };
-
-    haskell-nix = {
-      url = "github:input-output-hk/haskell.nix";
-      inputs.hackage.follows = "hackage";
-    };
   };
 
-  outputs = inputs: inputs.iogx.lib.mkFlake {
-    inherit inputs;
-    repoRoot = ./.;
-    outputs = import ./nix/outputs.nix;
-    systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
-  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix, CHaP }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        overlay = final: prev: {
+          haskell-nix = prev.haskell-nix // {
+            extraPkgconfigMappings = prev.haskell-nix.extraPkgconfigMappings // {
+              # String pkgconfig-depends names are mapped to lists of Nixpkgs
+              # package names
+              "libblst" = [ "blst" ];
+            };
+          };
+        };
 
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.iog.io"
-    ];
-    extra-trusted-public-keys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    ];
-    allow-import-from-derivation = true;
-  };
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            webkitgtk = final.webkitgtk_4_0;
+            libsodium = prev.libsodium.overrideAttrs (fAttrs: pAttrs: {
+              fAttrs.src = fetchGit {
+                url = "https://github.com/IntersectMBO/libsodium";
+                rev = "dbb48cce5429cb6585c9034f002568964f1ce567";
+              };
+            });
+            #with final; stdenv.mkDerivation rec {
+            #   pname = "libsodium";
+
+            #   src = fetchGit {
+            #     url = "https://github.com/IntersectMBO/libsodium";
+            #     rev = version;
+            #   };
+            #   version = "dbb48cce5429cb6585c9034f002568964f1ce567";
+
+            #   nativeBuildInputs = [ autoreconfHook ];
+
+            #   configureFlags = [ "--enable-static" ]
+            #     # Fixes a compilation failure: "undefined reference to `__memcpy_chk'". Note
+            #     # that the more natural approach of adding "stackprotector" to
+            #     # `hardeningDisable` does not resolve the issue.
+            #     ++ lib.optional stdenv.hostPlatform.isMinGW "CFLAGS=-fno-stack-protector";
+
+            #   outputs = [ "out" "dev" ];
+            #   separateDebugInfo = stdenv.isLinux && stdenv.hostPlatform.libc != "musl";
+
+            #   enableParallelBuilding = true;
+
+            #   doCheck = true;
+
+            #   meta = with lib; {
+            #     description = "A modern and easy-to-use crypto library - VRF fork";
+            #     homepage = "http://doc.libsodium.org/";
+            #     license = licenses.isc;
+            #     maintainers = [ "tdammers" "nclarke" ];
+            #     platforms = platforms.all;
+            #   };
+            # };
+          })
+          (final: prev: {
+            hixProject =
+              final.haskell-nix.project' {
+                src = ./.;
+                compiler-nix-name = "ghc966";
+                # This is used by `nix develop .` to open a shell for use with
+                # `cabal`, `hlint` and `haskell-language-server`
+                shell.tools = {
+                  cabal = { };
+                  hlint = { };
+                  haskell-language-server = { };
+                  fourmolu = { };
+                  cabal-fmt = { };
+                };
+
+                # Non-Haskell shell tools go here
+                shell.buildInputs = with final; [
+                  nixpkgs-fmt
+                ];
+
+                # ???: Fix for `nix flake show --allow-import-from-derivation`
+                evalSystem = "x86_64-linux";
+                inputMap = { "https://chap.intersectmbo.org/" = CHaP; };
+
+                modules = with final; [{
+                  packages.postgresql-libpq-configure.components.library.libs = lib.mkForce [ [ postgresql ] ];
+                }];
+
+              };
+          })
+          overlay
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.hixProject.flake { };
+      in
+      flake // {
+        legacyPackages = pkgs;
+        # Built by `nix build .`
+        packages.default = flake.packages."sky-atlas:exe:sky-atlas";
+      });
 }
