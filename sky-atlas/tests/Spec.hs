0cd8b9dd7cbc3bf31d0a4dfb3930cf6da4611319
Francois-Rene Rideau <fare@tunes.org>
2025-05-19 00:24:53 -0400

    Boilerplate for tests

--- sky-atlas/tests/Spec.hs
+++ sky-atlas/tests/Spec.hs
@@ -1,1 +1,10 @@
-module Spec where
+module Main (main) where
+
+import Test.Hspec
+-- import Spec.SkySpec (signatureSpec, fingerprintSpec, merkleSpec, topicSpec, bountySpec, bridgeSpec)
+
+main :: IO ()
+main = hspec $ do
+  describe "Signature Tests" $ do
+    return ()
+
