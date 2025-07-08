daef0ddb41e43538b46dac7e7096ec4f7f4cab89
Yaroslav Kozhevnikov <yroslav541+git@gmail.com>
2025-07-08 19:12:06 +0300

    feat: add minting policy construction to the tests

--- sky-atlas/tests/OnChain/ContractSpec.hs
+++ sky-atlas/tests/OnChain/ContractSpec.hs
@@ -2,6 +2,8 @@
 
 module OnChain.ContractSpec (contractSpec) where
 
+import API.SkyMintingPolicy
+import Contract.SkyBridge
 import Control.Monad.Extra (maybeM)
 import Data.Maybe (listToMaybe)
 import GeniusYield.HTTP.Errors
@@ -10,8 +12,9 @@ import GeniusYield.Test.Clb
 import GeniusYield.Test.Utils
 import GeniusYield.TxBuilder
 import GeniusYield.Types
+import PlutusLedgerApi.V1 (ScriptHash (..))
+import PlutusLedgerApi.V1.Value (CurrencySymbol (..))
 import Test.Tasty
-import Test.Tasty.HUnit
 
 -- | Test environment 'WalletInfo' among other things provides nine wallets that
 -- be used in tests. For convinience we assign some meaningful names to them.
@@ -27,23 +30,54 @@ contractSpec =
   testGroup
     "OnChain Contract Tests"
     [ testGroup
-        "Bridge Contract"
-        [ mkTestFor "simple transaction test" simpleTxTest,
-          testCase "should reject invalid committee signatures" $ do
-            assertFailure "Not Yet Implemented",
-          testCase "should validate NFT presence in outputs" $ do
-            assertFailure "Not Yet Implemented"
-        ],
-      testGroup
         "Sky Minting Policy"
-        [ testCase "should validate minting with correct public key hash" $ do
-            assertFailure "Not Yet Implemented",
-          testCase "should reject minting without proper signature" $ do
-            assertFailure "Not Yet Implemented",
-          testCase "should ensure exactly one token is minted" $ do
-            assertFailure "Not Yet Implemented"
+        [ mkTestFor "simple transaction test" simpleTxTest,
+          mkTestFor "minting policy" mintingPolicyTest
         ]
     ]
+
+mintingPolicyTest :: (GYTxGameMonad m) => TestInfo -> m ()
+mintingPolicyTest TestInfo {..} = do
+  asUser (admin testWallets) $ do
+    mintSkeleton <- mkMintingSkeleton
+    gyLogDebug' "" $ printf "tx skeleton: %s" (show mintSkeleton)
+    txId <- buildTxBody mintSkeleton >>= signAndSubmitConfirmed
+    gyLogDebug' "" $ printf "tx submitted, txId: %s" txId
+
+mkMintingSkeleton :: (GYTxUserQueryMonad m) => m (GYTxSkeleton 'PlutusV2)
+mkMintingSkeleton = do
+  addr <-
+    maybeM
+      (throwAppError $ someBackendError "No own addresses")
+      pure
+      $ listToMaybe
+        <$> ownAddresses
+  gyLogDebug' "" $ printf "ownAddr: %s" (show addr)
+
+  pkh <- addressToPubKeyHash' addr
+
+  let bridgeDatum = BridgeNFTDatum ""
+      tokenName = "SkyBridgeToken"
+      skyPolicy = skyMintingPolicy' $ pubKeyHashToPlutus pkh
+      skyToken = GYToken (mintingPolicyId skyPolicy) tokenName
+      amt = 1
+      curSym = CurrencySymbol $ getScriptHash $ scriptHashToPlutus $ scriptHash skyPolicy
+
+  bvAddr <- bridgeValidatorAddress $ BridgeParams curSym
+
+  -- skeleton for minting transaction
+  pure $
+    mustMint @'PlutusV2 (GYBuildPlutusScript $ GYBuildPlutusScriptInlined skyPolicy) unitRedeemer tokenName amt
+      <> mustHaveOutput
+        ( GYTxOut
+            { -- recipient is the bridge validator
+              gyTxOutAddress = bvAddr,
+              gyTxOutValue = valueSingleton skyToken amt,
+              gyTxOutDatum = Just (datumFromPlutusData bridgeDatum, GYTxOutUseInlineDatum),
+              gyTxOutRefS = Nothing
+            }
+        )
+      <> mustBeSignedBy pkh
 
 -- | Trace for a super-simple spending transaction. This function combines
 -- the  runner and the test for simplicity's sake.
