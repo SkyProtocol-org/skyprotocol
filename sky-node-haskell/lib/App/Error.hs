6024790a94f139bb6116bb7e722b4891ebce345b
Yaroslav Kozhevnikov <yroslav541+git@gmail.com>
2025-08-03 21:45:13 +0300

    chore: remove first haskell implementation

--- sky-node-haskell/lib/App/Error.hs
+++
@@ -1,9 +0,0 @@
-module App.Error where
-
--- | Top-level error type
-data AppError
-  = -- | Constructor for error propagation from the handler level.
-    HandlerError String
-  | -- | Constructor for errors in the node itself.
-    AppError String
-  deriving (Show)
